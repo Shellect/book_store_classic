23201f2e9921c52d223aaf7e5f9dac34ed092732
Your name <example@example.com>
2024-02-29 21:22:13 +0300

    add cookie

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const pug = require('pug');
 const fs = require("fs");
 const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const {writeFile} = require("fs/promises");
 
 function error_log(err) {
     let d = new Date();
@@ -14,6 +16,7 @@ function error_log(err) {
 const app = express();
 app.use('/media', express.static('media'));
 app.use(bodyParser.json());
+// app.use(cors())
 
 app.get('/', (req, res) => {
     fs.readFile(__dirname + "/books.json", (err, data) => {
@@ -38,8 +41,23 @@ app.get('/', (req, res) => {
  * @param {Request} req
  */
 app.post('/buy', (req, res) => {
+    // 1. Синхронная запись в файл
+    fs.writeFileSync("users_books.csv", "test");
+    // 2. Асинхронная запись в файл с функцией обратного вызова
+    // fs.writeFile("users_books.csv", "test", function(){
+    //     console.log("File has been writed");
+    // });
+    // 3. Promise
+    // const promise = writeFile("users_books.csv", "test");
+    // promise.then(() => console.log("File has been writed"));
+    
+    const userId = Math.ceil(Math.random() * 1000);
+    const d = new Date();
+    d.setHours(d.getHours() + 5);
+    const expires = d.toUTCString();
+    res.setHeader("Set-Cookie", "user=" + userId + "; expires=" + expires)
     res.send(req.body);
-})
+});
 
 app.get('/order', (req, res) => {
     let filepath = path.join(__dirname, "views", "order.pug");
@@ -56,5 +74,5 @@ app.get('/profile', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log(`Server started by address: http://localhost:3000`);
+    console.log(`Server started by address: http://bookshop.local:3000`);
 });
